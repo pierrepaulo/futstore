b5b0bdf07bfc5bf2111197930abab852c4f1876d
Pierre Paulo <pierresantista@hotmail.com>
2025-09-14 10:39:30 -0300

    fix(product-list): corrige responsividade em telas sm e md

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const ProductList = ({ list }: Props) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8">
       {list.map((item) => (
         <ProductItem key={item.id} data={item} />
       ))}
