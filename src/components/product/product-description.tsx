80e62afb4cf84eb8f20ed17265a0047aaa8a414b
Pierre Paulo <pierresantista@hotmail.com>
2025-09-16 07:52:47 -0300

    feat(ui/product): cria descrição do produto (product-description.tsx)

--- src/components/product/product-description.tsx
+++ src/components/product/product-description.tsx
@@ -1,9 +1,36 @@
 "use client";
 
+import Image from "next/image";
+import { useState } from "react";
+
 type Props = {
   text: string;
 };
 
 export const ProductDescription = ({ text }: Props) => {
-  return <div>...</div>;
+  const [opened, setOpened] = useState(true);
+  return (
+    <div className="bg-white border border-gray-200 px-7 md:px-12  mt-20">
+      <div
+        className={`flex justify-between py-7 items-center ${
+          opened ? "border-b" : "border-0"
+        } border-b border-gray-200`}
+      >
+        <div className="text-xl ">Informações do produto</div>
+        <div
+          onClick={() => setOpened(!opened)}
+          className="cursor-pointer size-12  border border-gray-200 flex justify-center items-center rounded-sm"
+        >
+          <Image
+            src={"/assets/ui/arrow-left-s-line.png"}
+            alt=""
+            width={24}
+            height={24}
+            className={`transition-all ${opened ? "rotate-0" : "rotate-180"}`}
+          />
+        </div>
+      </div>
+      {opened && <div className="text-gray-500 my-12">{text}</div>}
+    </div>
+  );
 };
