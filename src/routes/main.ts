c2554f62b95d716111b35fdb05a08f67abb58112
Pierre Paulo <pierresantista@hotmail.com>
2025-10-10 11:12:05 -0300

    feat(category): obter dados da categoria

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as bannerController from "../controllers/banner";
 import * as productController from "../controllers/product";
+import * as categoryController from "../controllers/category";
 
 export const routes = Router();
 
@@ -12,3 +13,7 @@ routes.get("/banners", bannerController.getBanners);
 routes.get("/products", productController.getProducts);
 routes.get("/product/:id", productController.getOneProduct);
 routes.get("/product/:id/related", productController.getRelatedProducts);
+routes.get(
+  "/category/:slug/metadata",
+  categoryController.getCategoryWhitMetadata
+);
--- src/services/category.ts
+++ src/services/category.ts
@@ -11,3 +11,32 @@ export const getCategory = async (id: number) => {
   });
   return category;
 };
+
+export const getGategoryBySlug = async (slug: string) => {
+  const category = await prisma.category.findFirst({
+    where: { slug },
+    select: {
+      id: true,
+      name: true,
+      slug: true,
+    },
+  });
+  return category;
+};
+
+export const getCategoryMetadata = async (id: number) => {
+  const metadata = await prisma.categoryMetadata.findMany({
+    where: { categoryId: id },
+    select: {
+      id: true,
+      name: true,
+      values: {
+        select: {
+          id: true,
+          label: true,
+        },
+      },
+    },
+  });
+  return metadata;
+};
