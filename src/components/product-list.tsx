9e8e9aca41d00cb867f517b5c9b69ed89a15a399
Pierre Paulo <pierresantista@hotmail.com>
2025-10-14 15:09:58 -0300

    fix(product-list): corrige responsividade das imagens

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -6,7 +6,7 @@ type Props = {
 };
 export const ProductList = ({ list }: Props) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8">
       {list.map((item) => (
         <ProductItem key={item.id} data={item} />
       ))}
