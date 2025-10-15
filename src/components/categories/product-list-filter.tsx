3f5da86c4d9a9ae4ab475eae27d9fa2c35a550ea
Pierre Paulo <pierresantista@hotmail.com>
2025-10-15 10:35:47 -0300

    feat(categories) exibir produtos e corrigir responsividade

--- src/components/categories/product-list-filter.tsx
+++ src/components/categories/product-list-filter.tsx
@@ -3,6 +3,8 @@
 import { useQueryString } from "@/hooks/use-querystring";
 import { ChangeEvent, useState } from "react";
 import { FilterGroup } from "./filter-group";
+import { data } from "@/data";
+import { ProductItem } from "../product-item";
 
 export const ProductListFitler = () => {
   const queryString = useQueryString();
@@ -19,7 +21,8 @@ export const ProductListFitler = () => {
       <div>
         <div className="flex flex-col md:flex-row gap-6 justify-between items-start md:items-center">
           <div className="text-3xl">
-            <strong>99</strong>Produtos
+            <strong>{data.products.length} </strong>Produto
+            {data.products.length != 1 ? "s" : ""}
           </div>
           <div className="w-full md:max-w-70 flex flex-row gap-5">
             <select
@@ -50,11 +53,10 @@ export const ProductListFitler = () => {
           <FilterGroup id="nacional" name="Nacional" />
           <FilterGroup id="internacional" name="Internacional" />
         </div>
-        <div className="flex-1 grid grid-cols-1 md:grid-cols-3">
-          <div>...</div>
-          <div>...</div>
-          <div>...</div>
-          <div>...</div>
+        <div className="flex-1 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+          {data.products.map((item) => (
+            <ProductItem key={item.id} data={item} />
+          ))}
         </div>
       </div>
     </div>
