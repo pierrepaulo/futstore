d4aa90a39ce41eef9279d8feb11bb8a87f435dcc
Pierre Paulo <pierresantista@hotmail.com>
2025-10-15 11:27:18 -0300

    feat(product): criar detalhes do product.

--- src/components/product/product-details.tsx
+++ src/components/product/product-details.tsx
@@ -1,10 +1,48 @@
 "use client";
 
 import { ProductComplete } from "@/types/product";
+import Image from "next/image";
 
 type Props = {
   product: ProductComplete;
 };
 export const ProductDetails = ({ product }: Props) => {
-  return <div>...</div>;
+  const addToCart = async () => {
+    //TODO: criar cart
+  };
+
+  return (
+    <div className="flex-1">
+      <div className="text-xs text-gray-500 mb-2">Cod {product.id}</div>
+      <div className="font-bold text-3xl mb-6">{product.label}</div>
+      <div className="font-bold text-4xl text-blue-600 mb-2">
+        R$ {product.price.toFixed(2)}
+      </div>
+      <div className="text-sm text-gray-500 mb-6">Em 12x no cartão</div>
+      <div className="flex gap-4">
+        <button
+          onClick={addToCart}
+          className="flex-1 max-w-xs py-4 px-8 bg-blue-600 text-white border-0 rounded-sm hover:opacity-80 cursor-pointer"
+        >
+          Adcionar ao carrinho
+        </button>
+        <div className="size-14 border border-gray-200 flex justify-center items-center rounded-sm cursor-pointer">
+          <Image
+            src={"/assets/ui/heart-3-line.png"}
+            alt=""
+            width={24}
+            height={24}
+          />
+        </div>
+        <div className="size-14 border border-gray-200 flex justify-center items-center rounded-sm cursor-pointer">
+          <Image
+            src={"/assets/ui/share-line.png"}
+            alt=""
+            width={24}
+            height={24}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
