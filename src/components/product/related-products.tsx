b18613fe63c9509ea0bf4783c07399460f6be05a
Pierre Paulo <pierresantista@hotmail.com>
2025-10-21 14:05:03 -0300

    feat(product): pegaprodutos relacionados

--- src/components/product/related-products.tsx
+++ src/components/product/related-products.tsx
@@ -1,16 +1,18 @@
 import { data } from "@/data";
 import { ProductList } from "../product-list";
+import { getRelatedProducts } from "@/actions/get-related-products";
 
 type Props = {
   id: number;
 };
 
-export const RealatedProduct = ({ id }: Props) => {
+export const RealatedProduct = async ({ id }: Props) => {
+  const products = await getRelatedProducts(id);
   return (
     <div className="mt-10">
       <h3 className="text-2xl">Você tambem vai gostar</h3>
       <div className="mt-9">
-        <ProductList list={data.products} />
+        <ProductList list={products} />
       </div>
     </div>
   );
