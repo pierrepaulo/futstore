941ed832f4351b283fc7cc22584c64cb0a5e820a
Pierre Paulo <pierresantista@hotmail.com>
2025-10-21 13:58:48 -0300

    feat(product): integra produto unico

--- src/app/(site)/product/[id]/page.tsx
+++ src/app/(site)/product/[id]/page.tsx
@@ -1,10 +1,11 @@
+import { getProductWithCategory } from "@/actions/get-product-with-category";
 import { ImageSlider } from "@/components/product/image-slider";
 import { ProductDescription } from "@/components/product/product-description";
 import { ProductDetails } from "@/components/product/product-details";
 import { RealatedProduct } from "@/components/product/related-products";
 import { RelatedProductsSekeleton } from "@/components/product/related-products-skeleton";
-import { data } from "@/data";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 type Props = {
@@ -13,11 +14,20 @@ type Props = {
 export default async function Page({ params }: Props) {
   const { id } = await params;
 
-  //TODO: fazer consulta pelo id
+  const data = await getProductWithCategory(parseInt(id));
+  if (!data) {
+    redirect("/");
+    return;
+  }
+
   return (
     <div>
       <div className="text-gray-500 mb-4">
-        <Link href={"/"}>Home</Link> &gt; <Link href={"/"}>Camisas</Link> &gt;
+        <Link href={"/"}>Home</Link> &gt;{" "}
+        <Link href={`/categories/${data.category.slug}`}>
+          {data.category.name}
+        </Link>{" "}
+        &gt;
         {data.product.label}
       </div>
 
